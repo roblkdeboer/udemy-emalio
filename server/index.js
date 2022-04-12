f43a4d60f1adf6f37d1bf81ad6e751cce209f905
Rob de Boer <roblkdeboer@gmail.com>
2022-04-12 21:06:22 +0800

    Add Google OAuth  Passport Strategy

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
+import 'dotenv/config';
 import express from 'express';
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there!!!' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accesstoken);
+    }
+  )
+);
 
 // Check which port to use
 const PORT = process.env.PORT || 5000;
