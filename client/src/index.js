9a9ab638c692c7b58902095836882f101b8a69b1
Rob de Boer <roblkdeboer@gmail.com>
2022-04-25 22:58:03 +0800

    Set up reducer and configure store

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = configureStore({
+  reducer: reducers,
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
