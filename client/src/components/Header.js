abba16096c2cf26f82a148cc9a287da242f56d47
Rob de Boer <roblkdeboer@gmail.com>
2022-04-27 21:45:10 +0800

    Add collapsible side bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 class Header extends Component {
+  componentDidMount() {
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, { edge: 'right' });
+  }
   render() {
     return (
-      <nav>
-        <div class="nav-wrapper">
-          <a href="/" className="brand-logo" style={{ 'margin-left': '1rem' }}>
-            Emalio
-          </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="/">Login With Google</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <>
+        <nav>
+          <div class="nav-wrapper">
+            <a
+              href="/"
+              className="brand-logo"
+              style={{ 'margin-left': '1rem' }}
+            >
+              Emalio
+            </a>
+            <a
+              href="/#"
+              data-target="mobile-demo"
+              className="sidenav-trigger right"
+            >
+              <i className="material-icons">menu</i>
+            </a>
+
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <a href="/">Login With Google</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <ul className="sidenav" id="mobile-demo">
+          <li>
+            <a href="/">Login With Google</a>
+          </li>
+        </ul>
+      </>
     );
   }
 }
